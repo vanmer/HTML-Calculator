d0f08ae7d601d252a69167e67fe0b8ed1658386a
vanmer <60948275+vanmer@users.noreply.github.com>
2020-06-09 14:03:05 +0200

    commit functionality of display, clear btn

--- app.js
+++ app.js
@@ -15,25 +15,54 @@ document.addEventListener('DOMContentLoaded', () => {
   var backspaceBtn = document.getElementById('calc-backspace');
   var displayValElement = document.getElementById('calc-display-val');
 
-  var displayVal = '0';
-  var pendingVal;
-  var evalStringArray = []; // holding values that are pressed
+  var displayVal = '0';     // display value on the screen
+  var pendingVal;           // pending value (previous value)
+  var evalStringArray = []; // holding operations that are pressed/passed
 
   var calcNumBtns = document.getElementsByClassName('calc-btn-num');
   var calcOperatorBtns = document.getElementsByClassName('calc-btn-operator');
 
   var updateDisplayVal = (clickObj) => {
     var btnText = clickObj.target.innerText;
-    if (displayValElement)
+
+    if (displayVal === '0') {
+          displayVal = '';
+    }
+    displayVal += btnText;
+    displayValElement.innerText = displayVal;
+
   }
 
-  // create Event Listeners for num and operator buttons
+  // Event Listeners for num and operator buttons
   for (let i = 0; i < calcNumBtns.length; i++) {
     calcNumBtns[i].addEventListener('click', updateDisplayVal, false);
   }
-  for (let i = 0; i < calcOperatorBtns.length; i++) {
-    calcOperatorBtns[i].addEventListener('click', performOperation, false);
+  // for (let i = 0; i < calcOperatorBtns.length; i++) {
+  //   calcOperatorBtns[i].addEventListener('click', performOperation, false);
+  // }
+
+  // clear Button function
+  clearBtn.onclick = () => {
+    displayVal = '0';
+    pendingVal = undefined;
+    evalStringArray = [];
+    displayValElement.innerHTML = displayVal;
   }
+
+  // backspace Button function
+  backspaceBtn.onclick = () => {
+    let lengthOfDisplayVal = displayVal.length;
+    displayVal = displayVal.slice(0, lengthOfDisplayVal - 1);
+
+    if (displayVal === '') {
+      displayVal = '0';
+    }
+
+    displayValElement.innerText = displayVal;
+
+  }
+
+
 
 
 
